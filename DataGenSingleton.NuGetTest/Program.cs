d21670b5315da6ad07b69416c0b7fb1f0be48f06
Karol Habczyk <khabczyk@yahoo.com>
2016-07-13 22:13:02 +0200

    NuGet test

---
+++ DataGenSingleton.NuGetTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using DataGen.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenSingleton.NuGetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = SingletonTest.Instance;
+            singleton1.Value = 13;
+
+            var singleton2 = SingletonTest.Instance;
+            singleton2.Value = 7;
+
+            Console.WriteLine(singleton1.Value);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class SingletonTest:Singleton<SingletonTest>
+    {
+        public int Value { get; set; }
+    }
+}
